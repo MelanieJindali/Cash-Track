106d4ab5b111b61d1cb3eb22b12db2c8b0bef707
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-10-23 09:26:11 -0500

    Moving Transaction into own component

--- cash-track/src/components/TransactionList.js
+++ cash-track/src/components/TransactionList.js
@@ -1,18 +1,16 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
+import { Transaction } from './Transaction'
 
 export const TransactionList = () => {
 	const { transactions } = useContext(GlobalContext)
-  
-  return (
+
+	return (
 		<div>
 			<h3>History</h3>
 			<ul>
 				{transactions.map((transaction) => (
-					<li>
-						{transaction.text} <span>-$400</span>
-						<button className='delete-btn'>x</button>
-					</li>
+					<Transaction key={transaction.id} transaction={transaction} />
 				))}
 			</ul>
 		</div>
