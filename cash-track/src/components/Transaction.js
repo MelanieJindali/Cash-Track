14eff69a6d3dd2f33f9a321a9302203bcbc6851a
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-10-23 09:40:51 -0500

    Add conditional for type transaction & styling

--- cash-track/src/components/Transaction.js
+++ cash-track/src/components/Transaction.js
@@ -4,8 +4,8 @@ export const Transaction = ({ transaction }) => {
 	const sign = transaction.amount < 0 ? '-' : '+'
 
 	return (
-		<li>
-			{transaction.text}{' '}
+		<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+			{transaction.text}
 			<span>
 				{sign}${Math.abs(transaction.amount)}
 			</span>
